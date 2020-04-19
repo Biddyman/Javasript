23bf836d06620e885bf286cfea08a160eca5047e
Biddyman <61958630+Biddyman@users.noreply.github.com>
2020-04-19 20:06:44 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function param (i)
+
+let i = 1;
+
+if (i % 2 = 0 i<=10 i++) {
+    return ('yu');
+} else {
+    return (i);
+}
+    if (i % 3 = 0 i<=n i++) {
+        return ('gi');
+    } else {
+        return (i);
+    
+}
+
+if (i % 5 = 0 i<=n i++) {
+    return ('oh');
+} else {
+    return (i);
+
+}
+
+if (i % 2 = 0 && i % 3 = 0 i<=n i++) {
+    return ('yu-gi');
+}   
+
+if (i % 2 = 0 && i % 5 = 0 i<=n i++) {
+    return ('yu-oh');
+}   
+
+if (i % 3 = 0 && i % 5 = 0 i<=n i++) {
+    return ('gi-oh');
+}   
+
+if (i % 2 = 0 && i % 3 = 0 && i % 5 = 0 i<=n i++) {
+    return ('yu-gi-oh');
+} 
+    
+console.log(i);
+
+
